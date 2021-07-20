fef370a844d8dfdb4ab2960d58599b6b86fed58b
Vithor <vithorpileco@gmail.com>
2021-07-20 13:04:26 -0300

    modularizing it

---
+++ macros_and_funcs.h
@@ -0,0 +1,45 @@
+#ifndef MACROS
+#define MACROS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+// STRING MACRO
+#define CHAR_SIZE 1000
+
+//  TIME MACROS
+#define MILISECONDS_LIMIT 1000
+#define SECONDS_LIMIT 60
+#define MINUTES_LIMIT 60
+#define NEGATIVE_LIMIT 0
+
+//  OUTPUT FILE NAME MACRO
+#define NAME_FILE_OUT "Legenda_sinc.srt"
+
+// STRUCT TIME MACROS
+#define TIME_STRUC(x) x.hours, x.minutes, x.seconds, x.miliseconds
+#define TIME_FORMAT "%02d:%02d:%02d,%03d"
+#define ISTIMENEGATIVE timefile.seconds < NEGATIVE_LIMIT || timefile.miliseconds < NEGATIVE_LIMIT || timefile.minutes < NEGATIVE_LIMIT \
+                || timefile.hours < NEGATIVE_LIMIT
+
+struct time{
+    int hours;
+    int minutes;
+    int seconds;
+    int miliseconds;
+};
+
+// Functions prototypes
+void subtract_measures(int upper_limit, int lower_limit, int *independent_measure, int *dependent_measure);
+struct time get_time(char *pointer);
+void subtract_time(struct time *time_file, struct time time_arg);
+void subtract_measures(int upper_limit, int lower_limit, int *independent_measure, int *dependent_measure);
+void isnegative(struct time timefile);
+void sync(FILE *arqinp, FILE *arqout, char *strptr, struct time time_arg);
+int myatoi(char *s, int *read_count);
+void error_reading(int s);
+
+#endif
--- srt_time_editor.c
+++ srt_time_editor.c
@@ -6,47 +6,7 @@ If you want to delay the paramaters must be negative, the contrary goes to advan
 If you won't use some time paramater just put it as a zero. 
 Example: sync legenda.srt 0 -2 -100 ( to delay all the subtitles of the legenda.srt file in two seconds and 100 miliseconds)*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <ctype.h>
-
-// STRING MACRO
-#define CHAR_SIZE 1000
-
-//  TIME MACROS
-#define MILISECONDS_LIMIT 1000
-#define SECONDS_LIMIT 60
-#define MINUTES_LIMIT 60
-#define NEGATIVE_LIMIT 0
-
-//  OUTPUT FILE NAME MACRO
-#define NAME_FILE_OUT "Legenda_sinc.srt"
-
-// STRUCT TIME MACROS
-#define TIME_STRUC(x) x.hours, x.minutes, x.seconds, x.miliseconds
-#define TIME_FORMAT "%02d:%02d:%02d,%03d"
-#define ISTIMENEGATIVE timefile.seconds < NEGATIVE_LIMIT || timefile.miliseconds < NEGATIVE_LIMIT || timefile.minutes < NEGATIVE_LIMIT \
-                || timefile.hours < NEGATIVE_LIMIT
-
-struct time{
-    int hours;
-    int minutes;
-    int seconds;
-    int miliseconds;
-};
-
-// Functions prototypes
-void subtract_measures(int upper_limit, int lower_limit, int *independent_measure, int *dependent_measure);
-struct time get_time(char *pointer);
-void subtract_time(struct time *time_file, struct time time_arg);
-void subtract_measures(int upper_limit, int lower_limit, int *independent_measure, int *dependent_measure);
-void isnegative(struct time timefile);
-void sync(FILE *arqinp, FILE *arqout, char *strptr, struct time time_arg);
-int myatoi(char *s, int *read_count);
-void error_reading(int s);
-
+#include "macros_and_funcs.h"
 
 int main(int argc, char *argv[]){
     char strptr[CHAR_SIZE];
@@ -69,11 +29,11 @@ int main(int argc, char *argv[]){
     
     // Exiting the function in case something goes wrong
     if (arqinp == NULL){
-        fprintf(stderr, "Error: Input file didn't open correctly");
+        fprintf(stderr, "Error: Input file couldn't be opened correctly");
         return EXIT_FAILURE;
     }
     if (arqout == NULL){
-        fprintf(stderr, "Error: Output file didn't open correctly");
+        fprintf(stderr, "Error: Output file couldn't be opened correctly");
         return EXIT_FAILURE;
     }
 
